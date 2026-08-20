e274e9e0b02497c4b8f4420123de3d19c78a53e5
Jiahan Chen <chenjiahan.jait@bytedance.com>
2026-08-20 16:43:43 +0800

    chore: migrate to Rstack CLI (#72)
    * chore: migrate to Rstack CLI
    
    * fix
    
    * fix: run check
    
    * fix

---
+++ rstack.config.ts
@@ -0,0 +1,22 @@
+import { define } from 'rstack';
+
+define.fmt({
+  singleQuote: true,
+  sortPackageJson: true,
+});
+
+define.lib({
+  dts: true,
+  syntax: 'es2023',
+});
+
+define.lint(({ globals, js, ts }) => [
+  js.configs.recommended,
+  ts.configs.recommendedTypeChecked,
+  {
+    files: ['test/**/src/**/*.{js,jsx}'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+]);
