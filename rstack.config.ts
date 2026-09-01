e20562afa94f1b468a832ab46c71d3336d60ae11
neverland <chenjiahan.jait@bytedance.com>
2026-09-01 16:43:54 +0800

    chore: update rstack to v0.7.1 and refine linting configuration

--- rstack.config.ts
+++ rstack.config.ts
@@ -16,13 +16,7 @@ define.staged({
   '*.{json,md,mdx,css,scss,less,html,yml,yaml}': 'rs fmt',
 });
 
-define.lint(({ globals, js, ts }) => [
+define.lint(({ js, ts }) => [
   js.configs.recommended,
   ts.configs.recommendedTypeChecked,
-  {
-    files: ['test/**/src/**/*.{js,jsx}'],
-    languageOptions: {
-      globals: globals.browser,
-    },
-  },
 ]);
